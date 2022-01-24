bda5199fcbd3ea7f916333a0bb950620266f976d
Andrea Zanin <99.zanin@gmail.com>
2022-01-24 22:40:17 +0100

    builds for amd64

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-env GOOS=windows go build -o build/generic_server.exe ./cmd
-env GOOS=darwin go build -o build/generic_server_macos ./cmd
-env GOOS=linux go build -o build/generic_server_linux ./cmd
+env GOOS=windows GOARCH=amd64 go build -o build/generic_server.exe ./cmd
+env GOOS=darwin GOARCH=amd64 go build -o build/generic_server_macos ./cmd
+env GOOS=linux GOARCH=amd64 go build -o build/generic_server_linux ./cmd
