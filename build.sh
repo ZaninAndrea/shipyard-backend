dd28633f8a829f1a94b4c2df3d8d18098a4b54c5
Andrea Zanin <andrea@igloo.ooo>
2023-11-22 19:52:26 +0100

    Added arm binaries

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 env GOOS=windows GOARCH=amd64 go build -o build/generic_server.exe ./cmd
 env GOOS=darwin GOARCH=amd64 go build -o build/generic_server_macos ./cmd
 env GOOS=linux GOARCH=amd64 go build -o build/generic_server_linux ./cmd
+env GOOS=linux GOARCH=arm64 go build -o build/generic_server_linux_arm ./cmd
